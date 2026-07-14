c444b4fc48000884b282a5cc033712e78464009a
sanjana864 <sanjana23077@gmail.com>
2026-07-14 16:05:18 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
@@ -0,0 +1,19 @@
+// Last updated: 14/07/2026, 16:05:17
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) &&
+               validate(node.right, node.val, max);
+    }
+}
