f1b2e8aea7b089f9e6d3e63c608d44caaf36249b
sanjana864 <sanjana23077@gmail.com>
2026-07-14 16:03:02 +0530

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.java
@@ -0,0 +1,17 @@
+// Last updated: 14/07/2026, 16:03:01
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (val < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
+        
+    
