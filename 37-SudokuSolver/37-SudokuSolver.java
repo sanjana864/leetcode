a49ad7403f9362d4a57a2eb65a9fad428f9b0cdb
sanjana864 <sanjana23077@gmail.com>
2026-07-14 16:05:59 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -0,0 +1,41 @@
+// Last updated: 14/07/2026, 16:05:57
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+
+            int r = 3 * (row / 3) + i / 3;
+            int cc = 3 * (col / 3) + i % 3;
+
+            if (board[r][cc] == c) return false;
+        }
+        return true;
+    }
+}
